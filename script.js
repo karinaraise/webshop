f98e51f73514ba4d9fe609fc7de91cfa51492c9b
karinaraise <karina_raise@hotmail.com>
2022-06-16 20:42:01 +0200

    product to cart js
    Product (illustratie op canvas) op single page product, kan op winkelwagen toegevoegd worden. totaal prijs wordt ook gerekend. Css nog niet aangepast.

--- script.js
+++ script.js
@@ -45,8 +45,93 @@ function showSlides(n) {
 } //tot hier foto slide
 
 //add product to cart codes
-let carts = documt.querySelectorAll('.material-symbols-outlined cart');
+let carts = document.querySelectorAll('.add-cart');
+let products = [
+  {
+    name:'Illustratie op canvas',
+    tag: 'canvas',
+    price: 32,
+    inCart: 0 
+  },
+  {
+    name:'Illustratie op poster',
+    tag: 'poster',
+    price: 27,
+    inCart: 0
+  },
+  {
+    name:'Illustratie op pet',
+    tag: 'pet',
+    price: 24,
+    inCart: 0
+  }
+];
 
 for (let i=0; i < carts.length; i++) {
-  console.log("my loop");
+  carts[i].addEventListener('click',() => {
+    cartNumbers(products[i]);
+    totalCost(products[i])
+  })
 }
+
+function onLoadCartNumbers (){
+  let productNumbers = localStorage.getItem('cartNumbers');
+
+  if(productNumbers) {
+    document.querySelector('.cart span').textContent = productNumbers;
+  }
+}
+
+function cartNumbers(product) {
+  let productNumbers = localStorage.getItem('cartNumbers');
+
+  productNumbers = parseInt(productNumbers); //om een 'nummer' te laten zien ipv van 'string'
+  if( productNumbers ) {
+    localStorage.setItem('cartNumbers', productNumbers + 1);
+    document.querySelector('.cart span').textContent = productNumbers + 1; //laat het aantal zien op menu header 
+  } else {
+    localStorage.setItem('cartNumbers', 1);
+    document.querySelector('.cart span').textContent = 1; 
+  }
+  
+  setItems(product);
+}
+
+function setItems(product) {
+  let cartItems = localStorage.getItem('productsInCart');
+  cartItems = JSON.parse(cartItems); //json naar js
+  
+  if(cartItems != null){
+    if (cartItems[product.tag] == undefined) {
+      cartItems = {
+        ...cartItems,
+        [product.tag]: product
+      }
+    }
+    cartItems[product.tag].inCart += 1;
+  } else {
+    product.inCart = 1;
+    cartItems = {
+      [product.tag]: product
+  }
+  }
+  localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+}
+
+function totalCost(product) {
+  let cartCost = localStorage.getItem('totalCost');
+
+  console.log("My cartCost is", cartCost);
+  console.log(typeof cartCost );
+
+  if(cartCost != null) {
+    cartCost = parseInt(cartCost); //convert string to number
+    localStorage.setItem("totalCost", cartCost + product.price);
+  } else {
+    localStorage.setItem("totalCost", product.price);
+  }
+
+
+}
+
+onLoadCartNumbers();
